a4bde513629013d504ccfc6cf3d9778f894df13d
Filipi Nunes <filipi.nunes10@gmail.com>
2025-09-29 16:20:59 -0300

    feat: create the photo-widget component

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,5 +1,49 @@
-import Text from "../components/text";
+import Container from "../components/container";
+import PhotoWidget from "../contexts/photos/components/photo-widget";
+import type { Photo } from "../contexts/photos/models/photo";
 
 export default function PageHome() {
-  return <Text>Home</Text>;
+  return (
+    <Container>
+      <div className="grid grid-cols-4 gap-9">
+        <PhotoWidget
+          photo={{
+            id: "1234",
+            title: "teste",
+            albums: [
+              { id: "1", title: "Albúm 1" },
+              { id: "2", title: "Albúm 2" },
+              { id: "3", title: "Albúm 3" },
+            ],
+            imageId: "portrait-tower.png",
+          }}
+        />
+        <PhotoWidget
+          photo={{
+            id: "5678",
+            title: "teste",
+            albums: [
+              { id: "1", title: "Albúm 1" },
+              { id: "2", title: "Albúm 2" },
+              { id: "3", title: "Albúm 3" },
+            ],
+            imageId: "portrait-tower.png",
+          }}
+        />
+        <PhotoWidget
+          photo={{
+            id: "9012",
+            title: "teste",
+            albums: [
+              { id: "1", title: "Albúm 1" },
+              { id: "2", title: "Albúm 2" },
+              { id: "3", title: "Albúm 3" },
+            ],
+            imageId: "portrait-tower.png",
+          }}
+        />
+        <PhotoWidget photo={{} as Photo} loading />
+      </div>
+    </Container>
+  );
 }
