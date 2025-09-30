9dd4e28ef03dbe00c330628e565cc0add62a23d2
Filipi Nunes <filipi.nunes10@gmail.com>
2025-09-30 18:26:06 -0300

    feat: creates the photos-list component

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,49 +1,54 @@
 import Container from "../components/container";
-import PhotoWidget from "../contexts/photos/components/photo-widget";
+import { PhotosList } from "../contexts/photos/components/photos-list";
 import type { Photo } from "../contexts/photos/models/photo";
+
+const photos: Photo[] = [
+  {
+    id: "1",
+    title: "teste",
+    albums: [
+      { id: "1", title: "Albúm 1" },
+      { id: "2", title: "Albúm 2" },
+      { id: "3", title: "Albúm 3" },
+    ],
+    imageId: "portrait-tower.png",
+  },
+  {
+    id: "2",
+    title: "teste",
+    albums: [
+      { id: "1", title: "Albúm 1" },
+      { id: "2", title: "Albúm 2" },
+      { id: "3", title: "Albúm 3" },
+    ],
+    imageId: "portrait-tower.png",
+  },
+  {
+    id: "3",
+    title: "teste",
+    albums: [
+      { id: "1", title: "Albúm 1" },
+      { id: "2", title: "Albúm 2" },
+      { id: "3", title: "Albúm 3" },
+    ],
+    imageId: "portrait-tower.png",
+  },
+  {
+    id: "4",
+    title: "teste",
+    albums: [
+      { id: "1", title: "Albúm 1" },
+      { id: "2", title: "Albúm 2" },
+      { id: "3", title: "Albúm 3" },
+    ],
+    imageId: "portrait-tower.png",
+  },
+];
 
 export default function PageHome() {
   return (
     <Container>
-      <div className="grid grid-cols-4 gap-9">
-        <PhotoWidget
-          photo={{
-            id: "1234",
-            title: "teste",
-            albums: [
-              { id: "1", title: "Albúm 1" },
-              { id: "2", title: "Albúm 2" },
-              { id: "3", title: "Albúm 3" },
-            ],
-            imageId: "portrait-tower.png",
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: "5678",
-            title: "teste",
-            albums: [
-              { id: "1", title: "Albúm 1" },
-              { id: "2", title: "Albúm 2" },
-              { id: "3", title: "Albúm 3" },
-            ],
-            imageId: "portrait-tower.png",
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: "9012",
-            title: "teste",
-            albums: [
-              { id: "1", title: "Albúm 1" },
-              { id: "2", title: "Albúm 2" },
-              { id: "3", title: "Albúm 3" },
-            ],
-            imageId: "portrait-tower.png",
-          }}
-        />
-        <PhotoWidget photo={{} as Photo} loading />
-      </div>
+      <PhotosList photos={photos} />
     </Container>
   );
 }
